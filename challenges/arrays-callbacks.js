e668fb4fd18c5575beb0fe3df6b35d5f677d5395
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-10-16 10:37:31 -0700

    all array challenges are done, now on eot callbacks

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -15,60 +15,60 @@ const zooAnimals = [
   { animal_name: "Australian pelican", population: 5, scientific_name: "Pelecanus conspicillatus", state: "West Virginia" },
 ];
 
-/* Request 1: .forEach()
+/* Request 1: .forEach()--DONE
 
-// The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
+The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
-// //come back to this one..
-// */
-// function getAnimalNames(func){
-//   const displayNames = [];
-//     func.forEach(element => {
-//       displayNames.push(element.animal_name);
-//       displayNames.push(element.scientific_name);
-//     });
-//   console.log(displayNames);
+//come back to this one..
+*/
+function getAnimalNames(func){
+  const displayNames = [];
+    func.forEach(element => {
+      displayNames.push(element.animal_name);
+      displayNames.push(element.scientific_name);
+    });
+  console.log(displayNames);
   
-//   }
-//   function stringName(func){
-//     func.forEach(item =>{
-//       console.log(`Name:${item.animal_name} Scientific:${item.scientific_name}`);
-//     })
-//   }
+  }
+  function stringName(func){
+    func.forEach(item =>{
+      console.log(`Name:${item.animal_name} Scientific:${item.scientific_name}`);
+    })
+  }
   
-//   stringName(zooAnimals);
-//   getAnimalNames(zooAnimals);
-// /* Request 2: .map()
+  stringName(zooAnimals);
+  getAnimalNames(zooAnimals);
+ /* Request 2: .map()--DONE
 
-// The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
+The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
-// */
+*/
 
 
-// const lowCaseAnimalNames =zooAnimals.map(item => item.animal_name.toLowerCase());
-// console.log(lowCaseAnimalNames);
+const lowCaseAnimalNames =zooAnimals.map(item => item.animal_name.toLowerCase());
+console.log(lowCaseAnimalNames);
 /* Request 3: .filter() 
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-//currently coming back with empty string --REVIEW
 
-// const lowPopulationAnimals = zooAnimals.filter((item)  =>  {return item.animal_name <= 5});
-// console.log(lowPopulationAnimals);
+const lowPopulationAnimals = zooAnimals.filter((item)  =>  {return item.population < 5});
+console.log(lowPopulationAnimals);
+
 
 
 /* Request 4: .reduce() //DONE
 
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
-*/
-const totalAnimalPopulation = zooAnimals.reduce((populationTotal, item) =>
-{
-  return populationTotal += item.population;
-}, 0);
+// */
+// const totalAnimalPopulation = zooAnimals.reduce((populationTotal, item) =>
+// {
+//   return populationTotal += item.population;
+// }, 0);
 
-console.log(totalAnimalPopulation);
+// console.log(totalAnimalPopulation);
 
 
 // ==== Callbacks ====  
@@ -78,7 +78,13 @@ console.log(totalAnimalPopulation);
   * The first two parameters can take any argument (we can pass any value as argument)
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
-*/
+// */
+//   function consume (a,b,cb){
+//     let a =a;
+//     let b = b;
+    
+//   };
+
 
 
 /* Step 2: Create several functions to callback with consume();
